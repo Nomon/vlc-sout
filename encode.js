7624e0e97ecf3db2f330ccff9fd4f3b4fb8caa6d
Matti Savolainen <matti@applifier.com>
2013-05-19 15:03:33 +0300

    CLI for encoding videos with vlc

---
+++ encode.js
@@ -0,0 +1,33 @@
+var argv = require('optimist')
+  .usage('Usage: $0 [mp4]')
+  .demand(['_'])
+  .argv;
+
+var child_process = require('child_process');
+var sout = require('./index.js');
+var options = [];
+options.push('-I');
+options.push('dummy');
+options.push('-vvvv')
+options.push(argv._[0]);
+options.push('vlc://quit');
+
+options.push('--video-filter=rotate --rotate-angle=180');
+options.push('--video-filter=transform --transform-type=vflip');
+options.push("--sout");
+options.push(sout.stringify(require('./test/data/basic.json')))
+
+console.log(options)
+var vlc = child_process.spawn('/Applications/VLC.app/Contents/MacOS/VLC',options);
+
+vlc.stdout.on('data', function (data) {
+  console.log(data.toString('utf8'));
+});
+
+vlc.stderr.on('data', function (data) {
+  console.error(data.toString('utf8'));
+});
+
+vlc.on('close', function (code) {
+  console.log('vlc process exited with code ' + code);
+});
